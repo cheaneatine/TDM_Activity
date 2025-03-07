bb4edf59eb8c6c4d468eef0f2ae35c7fdefbd775
zayqrashid <eroslucagbo314@gmail.com>
2025-03-07 22:12:07 +0800

    Convert to modular functions -Lead Developer: Eros Lucagbo

--- SalaryDeductionCalculator.py
+++ SalaryDeductionCalculator.py
@@ -1,19 +1,30 @@
-def compute_deductions(salary):
-    sss = 1200
+def compute_deductions(salary, sss, pagibig):
+    tax =  1875  #Assuming fixed value for simplicity. (gi hardcode nanako ang value since constant/fixed value nmn daw ni - eros)
     philhealth = (salary * 0.05) / 2
-    pagibig = 100
-    tax = 1875 # Assuming fixed value for simplicity
-
     deductions = sss + philhealth + pagibig + tax
     net_salary = salary - deductions
 
-    print("Gross Salary:", salary)
-    print("SSS Deduction:", sss)
-    print("PhilHealth Deduction:", philhealth)
-    print("Pag-IBIG Deduction:", pagibig)
-    print("Tax Deduction:", tax)
-    print("Total Deductions:", deductions)
-    print("Net Salary:", net_salary)
+    return [salary, sss, philhealth, pagibig, tax, deductions, net_salary]
+
+
+def display(results):
+
+    print("Gross Salary:", results[0])
+    print("SSS Deduction:", results[1])
+    print("PhilHealth Deduction:", results[2])
+    print("Pag-IBIG Deduction:", results[3])
+    print("Tax Deduction:", results[4])
+    print("Total Deductions:", results[5])
+    print("Net Salary:", results[6])
+
 
 salary = float(input("Enter your monthly salary: "))
-compute_deductions(salary)
+sss =  float(input("Enter SSS Deduction: "))
+pagibig =  float(input("Enter Pag-ibig Deduction: "))
+
+
+results = compute_deductions(salary, sss, pagibig)
+
+display(results)
+
+#Akong rang gi change kay gipahimoan nakog functions ang pag display, 
